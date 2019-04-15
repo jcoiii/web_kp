bd8319268bc881f68f9466672382c18061427dca
Jonathan Chandra <chandrajonathan5@gmail.com>
2019-04-16 01:36:13 +0700

    Login System Updated

---
+++ js/fireBase.js
@@ -0,0 +1,15 @@
+var app_fireBase = {};
+(function(){
+	
+	var config = {
+    apiKey: "AIzaSyAiaNcXbUMTKBEx6uUx0Gha6qgndadxkzI",
+    authDomain: "kerja-praktek-1550498404585.firebaseapp.com",
+    databaseURL: "https://kerja-praktek-1550498404585.firebaseio.com",
+    projectId: "kerja-praktek-1550498404585",
+    storageBucket: "kerja-praktek-1550498404585.appspot.com",
+    messagingSenderId: "873471051525"
+};
+firebase.initializeApp(config);
+
+app_fireBase = firebase;
+})()
